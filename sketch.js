186796f457ce34bebebfdab94abe96a0935efc28
akshatkhann <73297038+akshatkhann@users.noreply.github.com>
2021-01-17 12:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var ball,hypnoticball,database,position;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+    hypnoticball = createSprite(250,250,10,10);
+    hypnoticball.shapeColor = "red";
+    var hypnoticposition=database.ref("ball/position")
+hypnoticposition.on("value",readposition,showerror)
+
+}
+
+function draw(){
+    background("white");
+if(position!==undefined){
+
+
+
+
+
+    if(keyDown(LEFT_ARROW)){
+        writeposition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writeposition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writeposition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writeposition(0,+1);
+    }
+    drawSprites();
+}
+}
+function readposition(data){
+position=data.val()
+hypnoticball.x=position.x
+hypnoticball.y=position.y
+
+}
+function writeposition(x,y){
+database.ref("ball/position").set({
+
+    x:position.x+x,
+    y:position.y+y
+
+
+
+})
+
+
+
+
+
+
+
+}
+function  showerror(){
+
+console.log("there was a problem reading from database")
+
+
+}
+
+
+
+
+
